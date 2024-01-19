1dd0a1b067dda80d7a0ba81700227595892f1b31
Lukas Matejovsky <lukyn.ma@gmail.com>
2024-01-19 19:06:59 +0100

    Vytvoření zobrazovacího řetězce

--- src/renderer/WiredRenderer.java
+++ src/renderer/WiredRenderer.java
@@ -2,8 +2,10 @@ package renderer;
 
 import rasterize.LineRasterizer;
 import solid.Solid;
+import transforms.Col;
 import transforms.Mat4;
 import transforms.Point3D;
+import transforms.Vec3D;
 
 import java.awt.*;
 
@@ -16,7 +18,11 @@ public class WiredRenderer {
         this.lineRasterizer = lineRasterizer;
     }
 
-    public void render(Solid solid) {
+    public void render(Solid solid, int width, int height, String projType) {
+        Color[] colors = new Color[5];
+        colors[0] = Color.GREEN;
+        colors[2] = Color.BLUE;
+        colors[4] = Color.YELLOW;
         for (int i = 0; i < solid.getIb().size(); i += 2) {
             int indexA = solid.getIb().get(i);
             int indexB = solid.getIb().get(i + 1);
@@ -36,18 +42,41 @@ public class WiredRenderer {
             a = a.mul(proj);
             b = b.mul(proj);
 
-            // TODO: ořezání
+            // Ořezání
+            if (((-a.getW()) <= a.getX() && a.getX() <= a.getW()) && ((-a.getW()) <= a.getY() && a.getY() <= a.getW()) && (0 <= a.getZ() && a.getZ() <= a.getW())) {
+                if (((-b.getW()) <= b.getX() && b.getX() <= b.getW()) && ((-b.getW()) <= b.getY() && b.getY() <= b.getW()) && (0 <= b.getZ() && b.getZ() <= b.getW())) {
+                    Vec3D v = new Vec3D();
+                    Vec3D w = new Vec3D();
+                    if (a.dehomog().isPresent())
+                        v = a.dehomog().get();
+                    if (b.dehomog().isPresent())
+                        w = b.dehomog().get();
 
-            // TODO: dehomogenizace
+                    Vec3D newIndexA = v.mul(new Vec3D(1, -1, 1));
+                    newIndexA = newIndexA.add(new Vec3D(1, 1, 0));
+                    newIndexA = newIndexA.mul(new Vec3D((double) (width - 1) / 2, (double) (height - 1) / 2, 1));
 
-            // TODO: tranformace do okna obrazovky
+                    Vec3D newIndexB = w.mul(new Vec3D(1, -1, 1));
+                    newIndexB = newIndexB.add(new Vec3D(1, 1, 0));
+                    newIndexB = newIndexB.mul(new Vec3D((double) (width - 1) / 2, (double) (height - 1) / 2, 1));
 
-            // Rasterizace
-            lineRasterizer.rasterize(
-                    (int)Math.round(a.getX()), (int)Math.round(a.getY()),
-                    (int)Math.round(b.getX()), (int)Math.round(b.getY()),
-                    Color.RED
-            );
+                    if (solid.getType() == "Solid") {
+                        // Rasterizace
+                        lineRasterizer.rasterize(
+                                (int) Math.round(newIndexA.getX()), (int) Math.round(newIndexA.getY()),
+                                (int) Math.round(newIndexB.getX()), (int) Math.round(newIndexB.getY()),
+                                solid.getColor()
+                        );
+                    } else {
+                        // Rasterizace
+                        lineRasterizer.rasterize(
+                                (int) Math.round(newIndexA.getX()), (int) Math.round(newIndexA.getY()),
+                                (int) Math.round(newIndexB.getX()), (int) Math.round(newIndexB.getY()),
+                                colors[i]
+                        );
+                    }
+                }
+            }
         }
     }
 
