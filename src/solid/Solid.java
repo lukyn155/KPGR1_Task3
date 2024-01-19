27db5cb1b1f18ce5b269ccf89da8147219280077
Lukas Matejovsky <lukyn.ma@gmail.com>
2024-01-19 19:02:45 +0100

    Upravení třídy solid pro zobrazení barev

--- src/solid/Solid.java
+++ src/solid/Solid.java
@@ -4,13 +4,17 @@ import transforms.Mat4;
 import transforms.Mat4Identity;
 import transforms.Point3D;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public abstract class Solid {
     protected ArrayList<Point3D> vb = new ArrayList<>();
     protected ArrayList<Integer> ib = new ArrayList<>();
+    protected String type = "Solid";
     private Mat4 model = new Mat4Identity();
+
+    protected Color color = Color.RED;
 
     protected void addIndices(Integer... indices) {
         ib.addAll(Arrays.asList(indices));
@@ -27,6 +31,12 @@ public abstract class Solid {
     public Mat4 getModel() {
         return model;
     }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getType() {return type;}
 
     public void setModel(Mat4 model) {
         this.model = model;
